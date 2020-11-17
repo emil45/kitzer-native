5a8e17f0f43c41a23dc6a89a60602df7a587184d
Emanueli <emanueli@diagnosticrobotics.com>
2020-11-17 10:05:19 +0200

    Make app working with async function and typescript

--- Kitzer/App.tsx
+++ Kitzer/App.tsx
@@ -1,60 +1,145 @@
 import styled from 'styled-components/native';
-import React, {useState} from 'react';
+import React, {useEffect, useState} from 'react';
 import Article from './components/article.component';
+import {parse} from 'fast-xml-parser';
 
-export interface NewsSite {
+interface INewsSite {
+  id: number;
   name: string;
   displayName: string;
   rssUrl: string;
   icon: {};
+  articles: IArticle[];
 }
 
-const newsSitesList: NewsSite[] = [
+export interface IArticle {
+  textAll: string;
+  time: string;
+  fuuTime: string;
+  picture: {};
+  text: string;
+  icon: {};
+  displayName: INewsSite['displayName'];
+}
+
+const newsSitesList: INewsSite[] = [
   {
+    id: 1,
     name: 'n12',
     displayName: 'N12 – אתר החדשות של ישראל',
     rssUrl:
       'https://rcs.mako.co.il/rss/31750a2610f26110VgnVCM1000005201000aRCRD.xml',
     icon: require('./assets/icons/mako.png'),
+    articles: [],
   },
   {
+    id: 2,
     name: 'bhol',
     displayName: 'בחדרי חרדים',
     rssUrl: 'https://www.bhol.co.il/rss/index.xml',
     icon: require('./assets/icons/bhol.png'),
+    articles: [],
   },
 ];
 
 export interface NewsSitesContainerProps {
-  newsSites: NewsSite[];
+  newsSites: INewsSite[];
 }
+
+const Title = styled.Text`
+  margin-top: 15px;
+  text-align: center;
+  font-size: 30px;
+  color: darkblue;
+`;
 
 const Container = styled.ScrollView`
   margin: 15px;
 `;
 
 const NewsSiteContainer = styled.View`
-  margin-bottom: 20px;
+  margin-top: 30px;
 `;
 
 const App: React.FC = () => {
   const [newsSites, setNewsSites] = useState(newsSitesList);
+  // const [articles, setArticles] = useState([]);
 
-  const renderNewsSiteArticles = (newsSite: NewsSite) => {
-    return <Article newsSite={newsSite} />;
+  useEffect(() => {
+    getArticles();
+  }, [newsSites]);
+
+  const getArticles = async () => {
+    let newNewsSites: INewsSite[] = [];
+
+    for (const newsSite of newsSites) {
+      await fetch(newsSite.rssUrl)
+        .then((response) => response.text())
+        .then((textResponse) => {
+          let obj = parse(textResponse);
+          let allItems = obj.rss.channel.item.slice(0, 10);
+          for (const item of allItems) {
+            let latest = item.title
+              .toString()
+              .replace(/&quot;/g, '"')
+              .replace(/&#39;/g, "'");
+            let time = item.pubDate.match(/([01][0-9]|2[0-3]):[0-5][0-9]/g);
+            newsSite.articles.push({
+              textAll: `${time} - ${latest}`,
+              time: time[0].replace(':', ''),
+              fuuTime: item.pubDate,
+              picture: item.picture,
+              text: latest,
+              icon: newsSite.icon,
+              displayName: newsSite.displayName,
+            });
+          }
+        });
+      newNewsSites.push(newsSite);
+    }
+    setNewsSites(newNewsSites);
   };
 
-  const renderNewsSitesContainers = (newsSites: NewsSite[]) => {
+  // async function getArticles() {
+  //   for (const newsSite of newsSites) {
+  //     const response = await fetch(newsSite.rssUrl);
+  //     response.json().then((res) => console.log(res));
+  //   }
+  // }
+  // for (const site of newsSites) {
+  //   const result = await getArticles(site);
+  //   console.log(result[0]);
+  //   allResults = [...allResults, ...result];
+  // }
+  // allResults.sort(function (a, b) {
+  //   return b.time - a.time;
+  // });
+  // setArticles(allResults);
+  // }
+
+
+  const renderNewsSiteArticles = (newsSite: INewsSite) => {
+    return newsSite.articles.map((article, index) => {
+      return <Article article={article} key={index} />;
+    });
+  };
+
+  const renderNewsSites = () => {
     return newsSites.map((newsSite) => {
       return (
-        <NewsSiteContainer>
+        <NewsSiteContainer key={newsSite.id}>
           {renderNewsSiteArticles(newsSite)}
         </NewsSiteContainer>
       );
     });
   };
 
-  return <Container>{renderNewsSitesContainers(newsSites)}</Container>;
+  return (
+    <Container>
+      <Title>Kitzer</Title>
+      {renderNewsSites()}
+    </Container>
+  );
 };
 
 export default App;
--- Kitzer/components/article.component.tsx
+++ Kitzer/components/article.component.tsx
@@ -1,15 +1,17 @@
 import React from 'react';
-import {NewsSite} from '../App';
+import {IArticle} from '../App';
 import styled from 'styled-components/native';
 
 export interface ArticleProps {
-  newsSite: NewsSite;
+  article: IArticle;
+  key: number;
 }
 
 const ArticleContainer = styled.View`
   height: 150px;
   border-radius: 8px;
   elevation: 4;
+  margin-bottom: 20px;
 `;
 
 const NewsSiteIcon = styled.Image`
@@ -30,14 +32,35 @@ const ArticleInfo = styled.View`
   position: absolute;
 `;
 
+const ArticleImage = styled.Image`
+  margin-left: 15px;
+  margin-bottom: 20px;
+  width: 300px;
+  height: 100px;
+`;
+
+const ArticleText = styled.Text`
+  color: grey;
+  margin-top: 20px;
+  margin-bottom: 12px;
+  margin-right: 10px;
+  margin-left: 10px;
+  direction: rtl;
+`;
+
 const Article: React.FC<ArticleProps> = (props) => {
-  const icon = require('../assets/icons/bhol.png');
+  // const icon = require('../assets/icons/bhol.png');
 
   return (
     <ArticleContainer>
+      <ArticleText>{props.article.textAll}</ArticleText>
+      {props.article.picture !== undefined && props.article.picture !== '' && (
+        <ArticleImage source={props.article.picture} />
+      )}
+
       <ArticleInfo>
-        <NewsSiteIcon source={icon} />
-        <NewsSiteName>{props.newsSite.displayName}</NewsSiteName>
+        <NewsSiteIcon source={props.article.icon} />
+        <NewsSiteName>{props.article.displayName}</NewsSiteName>
       </ArticleInfo>
     </ArticleContainer>
   );
